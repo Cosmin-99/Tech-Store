b7c1cb0bc3afd39e52d537358eff21a7ff2f0818
Cosmin <susara.cosmin@gmail.com>
2021-04-10 16:15:36 +0300

    Add login to app with Facebook account (backend)

---
+++ server/src/controllers/facebookLoginController.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from "express";
+import fetch from "node-fetch";
+import { QueryResult } from "pg";
+import { pool } from "../database/database";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+export const facebookLogin = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { userID, accessToken } = req.body;
+
+        const urlGraphFacebook = `https://graph.facebook.com/v2.11/${userID}/?fields=id,name,email&access_token=${accessToken}`
+        const response = await fetch(urlGraphFacebook, {
+            method: "GET"
+        });
+
+        const data = await response.json()
+
+        const { email, name } = data;
+        const user: QueryResult = await pool.query("SELECT * FROM Users WHERE email LIKE $1", [email]);
+        if (user.rows.length) {// if he is already in database
+            const token = jwt.sign({// create a login token
+                email: user.rows[0].email,
+                firstName: user.rows[0].firstName,
+                lastName: user.rows[0].lastName
+            },
+                process.env.TOKEN_ENCRYPTION as string)
+
+            return res.status(200).json({
+                firstName: user.rows[0].firstName,
+                lastName: user.rows[0].lastName,
+                email: email,
+                token
+            })
+        } else {// if is not into the database, will create credentials for that user
+            const lastName = name.substr(0, name.indexOf(" "));
+            const firstName = name.substr(name.indexOf(" ") + 1);
+            const password = email as string + process.env.TOKEN_ENCRYPTION as string;
+            const hashedPassword = bcrypt.hash(password, 12);
+
+            const newUser: QueryResult = await pool.query(`
+    INSERT INTO Users ("firstName", "lastName", "email", "password") VALUES ($1, $2, $3, $4)
+    `,
+                [
+                    firstName,
+                    lastName,
+                    email,
+                    hashedPassword
+                ])
+
+            const token = jwt.sign({
+                firstName: firstName,
+                lastName: lastName,
+                email: email
+            },
+                process.env.TOKEN_ENCRYPTION as string,
+            )
+
+            return res.status(200).json({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                token
+            })
+        }
+    } catch (err) {
+        return res.status(200).json(err)
+    }
+
+}
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { userLogin, userRegister } from "../controllers/authControllers";
+import { facebookLogin } from "../controllers/facebookLoginController";
 import { googleLogin } from "../controllers/googleLoginController";
 
 export const authRouter: Router = Router();
 
 authRouter.post("/register",userRegister);
 authRouter.post("/login",userLogin);
-authRouter.post("/google-login", googleLogin)
+authRouter.post("/google-login", googleLogin);
+authRouter.post("/facebook-login", facebookLogin);
