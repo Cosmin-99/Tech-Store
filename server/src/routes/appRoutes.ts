3efd74627eb75fc8f677bac858073451a16fd9a5
Cosmin <susara.cosmin@gmail.com>
2021-04-30 17:33:50 +0300

    Forgot to add routes :)

--- server/src/routes/appRoutes.ts
+++ server/src/routes/appRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import multer, { Multer, StorageEngine } from "multer";
+import { providerRegister } from "../controllers/addProviders";
 import { addCategory, getCategories, getSubcategoryById } from "../controllers/appControllers";
+import { sendEmailProviders } from "../controllers/sendEmailController";
 import { tokenMiddleWare } from "../utils/tokenMiddleware";
 
 export const appRoutes: Router = Router();
@@ -10,3 +12,5 @@ const singleFileUpload: Multer = multer({ storage: inMeoryStorage })
 appRoutes.post("/add-category", singleFileUpload.single('image'), addCategory)
 appRoutes.get("/categories", getCategories);
 appRoutes.get("/sub-categories/:id", getSubcategoryById);
+appRoutes.post("/provider", sendEmailProviders);
+appRoutes.post("/add-providers", providerRegister);
