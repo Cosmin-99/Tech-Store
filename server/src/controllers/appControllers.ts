17289c9cc2328346bc2b7fbec03f8d3d07de44d3
Cosmin <susara.cosmin@gmail.com>
2021-05-09 16:58:04 +0300

    Add category controllers for update, delete , get by id

--- server/src/controllers/appControllers.ts
+++ server/src/controllers/appControllers.ts
@@ -1,8 +1,10 @@
 import { Response, Request, NextFunction } from 'express'
 import { pool } from '../database/database'
-import azureStorage from 'azure-storage'
+import azureStorage, { BlobService } from 'azure-storage'
 import getStream from 'into-stream'
 import { QueryResult } from 'pg';
+import { ApiError } from '../error/ApiError';
+import { HttpStatusCode } from '../error/HttpStatusCodes';
 interface MulterRequest extends Request {
     file: any;
 }
@@ -68,7 +70,7 @@ export const getSubcategories = async (req: Request, res: Response, next: NextFu
     }
 }
 
-export const getSubcategoryById = async (req: Request, res: Response, next: NextFunction) => {
+export const getSubcategoryByCategoryId = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const id = +req.params.id;
 
@@ -93,5 +95,85 @@ export const getSubcategoryById = async (req: Request, res: Response, next: Next
         });
     } catch (e) {
         next(e);
+    }
+}
+
+export const updateCatergory = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    try {
+        const azureStorageConfig = {
+            accountName: process.env.ACCOUNT_NAME as string,
+            accountKey: process.env.ACCOUNT_KEY as string,
+            blobURL: process.env.BLOB_URL as string,
+            containerName: process.env.CONTAINER_NAME as string
+        }
+
+        const { name } = req.body;
+        const id: number = parseInt(req.params.id);
+        const request = (req as MulterRequest)
+
+        if (request.file !== undefined) {
+
+            const blobName = request.file.originalname;
+            const stream = getStream(request.file.buffer);
+            const streamLength = request.file.buffer.length;
+
+            const blobService: BlobService = azureStorage.createBlobService(azureStorageConfig.accountName, azureStorageConfig.accountKey);
+
+            blobService.createBlockBlobFromStream(azureStorageConfig.containerName, `${blobName}`, stream, streamLength, err => {
+                if (err) {
+                    return res.status(404).json(err);
+                } else {
+                    return res.status(200).json({
+                        filename: blobName,
+                        originalname: request.file.originalname,
+                        size: streamLength,
+                        path: `${azureStorageConfig.containerName}/${blobName}`,
+                        url: `${azureStorageConfig.blobURL}/${blobName}`
+                    });
+                }
+            });
+
+            const imageURL: string = `${azureStorageConfig.blobURL}/${blobName}`
+            const response: QueryResult = await pool.query('UPDATE Categories SET "name" = $1, "imageurl" = $2 WHERE id = $3',
+                [name, imageURL, id]);
+
+            return res.status(200).json({
+                message: "Category updated succesfully !!!"
+            })
+        } else {
+            const response: QueryResult = await pool.query('UPDATE Categories SET "name" = $1 WHERE id = $2',
+                [name, id]);
+
+            return res.status(200).json({
+                message: "Category updated succesfully !!!"
+            })
+        }
+
+    } catch (err) {
+        next(new ApiError(HttpStatusCode.BadRequest, err));
+    }
+}
+
+export const getCategoryById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id: number = parseInt(req.params.id)
+        const response: QueryResult = await pool.query("SELECT * FROM Categories WHERE id = $1",[id]);
+
+        return res.status(200).json(response.rows)
+    } catch(err) {
+        next(new ApiError(HttpStatusCode.BadRequest, err));
+    }
+}
+
+export const deleteCategory = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    try {
+        const id: number = parseInt(req.params.id)
+        const response: QueryResult = await pool.query("DELETE FROM Categories WHERE id = $1",[id]);
+
+        return res.status(200).json({
+            message: "Category deleted !!!"
+        })
+    } catch(err) {
+        next(new ApiError(HttpStatusCode.BadRequest, err));
     }
 }
--- server/src/routes/appRoutes.ts
+++ server/src/routes/appRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer, { Multer, StorageEngine } from "multer";
 import { providerRegister } from "../controllers/addProviders";
-import { addCategory, getCategories, getSubcategories, getSubcategoryById } from "../controllers/appControllers";
+import { addCategory, deleteCategory, getCategories, getCategoryById, getSubcategories, getSubcategoryByCategoryId, updateCatergory } from "../controllers/appControllers";
 import { sendEmailProviders } from "../controllers/sendEmailController";
 import { tokenMiddleWare } from "../utils/tokenMiddleware";
 
@@ -10,8 +10,11 @@ const inMeoryStorage: StorageEngine = multer.memoryStorage();
 const singleFileUpload: Multer = multer({ storage: inMeoryStorage })
 
 appRoutes.post("/add-category", singleFileUpload.single('image'), addCategory)
+appRoutes.put("/update-category/:id",singleFileUpload.single('image'),updateCatergory);
 appRoutes.get("/categories", getCategories);
-appRoutes.get("/sub-categories/:id", getSubcategoryById);
+appRoutes.get("/sub-categories/:id", getSubcategoryByCategoryId);
 appRoutes.get("/sub-categories", tokenMiddleWare, getSubcategories);
 appRoutes.post("/provider", sendEmailProviders);
 appRoutes.post("/add-providers", providerRegister);
+appRoutes.get("/category/:id", getCategoryById);
+appRoutes.delete("/category/:id", deleteCategory);
